9a7fa5dd4124407698b2569091640d9d71df1d10
J4VITXU <alu.156505@usj.es>
2025-09-22 18:07:20 +0200

    update

---
+++ processor.py
@@ -0,0 +1,24 @@
+import os
+import logging
+import requests
+import zipfile
+
+dowload_uris = [
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q1.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q2.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q3.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q4.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2020_Q1.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip"]
+
+DOWNLOAD_DIR = "downloads"
+
+def main() -> None:
+    response = requests.get("https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip")
+    print(response.content)
+    pass
+
+
+if __name__ == "__main__":
+    main()
